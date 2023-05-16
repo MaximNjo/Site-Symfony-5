dd283f92f48fe5985525c02566a276b42f859137
«MaximeNjo <maximenjo26@outlook.com>
2023-05-16 17:02:43 +0200

    milieu de la video 10

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,19 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="app_contact")
      */
-    public function listeContacts()
+    public function listeContact()
     {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+        $manager=$this->getDoctrine()->getManager();
+        $repo=$manager->getRepository(Contact::class);
+        $Contacts=$repo->findAll();
+        return $this->render('contacts/listeContacts.html.twig',[
+
+            'lesContacts' => $Contacts
         ]);
     }
 }
